28a7d8af6d695a031ccc8740f22a627920fcb969
Allen Reese <areese@yahoo-inc.com>
2016-02-29 13:07:14 -0800

    Checkpoint test

--- src/main/java/com/yahoo/monkey/TestCopyFrom.java
+++ src/main/java/com/yahoo/monkey/TestCopyFrom.java
@@ -1,5 +1,7 @@
 package com.yahoo.monkey;
 
+import java.util.Arrays;
+
 import sun.misc.Unsafe;
 
 import com.yahoo.jmh.jmhtest.jni.JNIWrapper;
@@ -12,7 +14,36 @@ public class TestCopyFrom {
      * JDK uses inotify and reads from a byte array. Let's try that.
      */
     public static void main(String[] args) {
+        // testReadingAndWritingNative();
+        testCopyFromByteArray();
+    }
 
+    private static void testCopyFromByteArray() {
+        Unsafe unsafe = MUnsafe.getUnsafe();
+        long size = 1024;
+        long address = unsafe.allocateMemory(size);
+
+        byte[] from = new byte[(int) size];
+        Arrays.fill(from, (byte) 0xDA);
+
+        unsafe.copyMemory(from, 0, null, address, size);
+
+//        for (int i = 0; i < size / 4; i += 4) {
+//            byte b = unsafe.getByte(address + i);
+//            System.out.print(Integer.toHexString(b));
+//        }
+//        System.out.println("");
+
+
+
+        JNIWrapper.dumpBytesFromAddress(address, 32);
+        long byteArrayBaseOffset = unsafe.arrayBaseOffset(byte[].class);
+        long byteArrayIndexScale = unsafe.arrayIndexScale(byte[].class);
+
+        System.err.println(byteArrayBaseOffset);
+    }
+
+    private static void testReadingAndWritingNative() {
         // we need an unsafe.
 
         Unsafe unsafe = MUnsafe.getUnsafe();
