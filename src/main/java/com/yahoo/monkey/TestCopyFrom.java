ef2a3d097ea437e95262e22b6901382475ed779c
Allen Reese <areese@yahoo-inc.com>
2016-02-29 10:18:20 -0800

    Checkpoint messing around with Unsafe, trying to understand how it works and build an example similar to how the jdk does it

---
+++ src/main/java/com/yahoo/monkey/TestCopyFrom.java
@@ -0,0 +1,11 @@
+package com.yahoo.monkey;
+
+public class TestCopyFrom {
+
+    /**
+     * JDK uses inotify and reads from a byte array. Let's try that.
+     */
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/yahoo/monkey/TestUnsafe.java
+++ src/main/java/com/yahoo/monkey/TestUnsafe.java
@@ -1,8 +1,9 @@
 package com.yahoo.monkey;
 
-import java.lang.reflect.Field;
-
 import sun.misc.Unsafe;
+
+import static com.yahoo.wildwest.MUnsafe.*;
+import com.yahoo.wildwest.MUnsafe;
 
 // http://www.docjar.com/docs/api/sun/misc/Unsafe.html#getInt
 
@@ -14,12 +15,12 @@ public class TestUnsafe {
 
         Unsafe unsafe = MUnsafe.getUnsafe();
 
-        System.err.println(new String(from));
-        //
-        for (int i = 0; i < from.length; i++) {
-            char c = unsafe.getChar(from, MUnsafe.charArrayOffset(i));
-            System.err.println(Integer.toHexString(c) + " " + c);
-        }
+        // System.err.println(new String(from));
+        // //
+        // for (int i = 0; i < from.length; i++) {
+        // char c = unsafe.getChar(from, MUnsafe.charArrayOffset(i));
+        // System.err.println(Integer.toHexString(c) + " " + c);
+        // }
         // for (int i = 0; i < from.length; i++) {
         // char c = unsafe.getChar(from, MUnsafe.charArrayOffset(i));
         // System.err.println(Integer.toHexString(c));
@@ -27,87 +28,38 @@ public class TestUnsafe {
 
 
         long memorySize = MUnsafe.charArraySize(from);
-        long newMemory = unsafe.allocateMemory(memorySize);
+        long newMemory = unsafe.allocateMemory(512 + memorySize);
 
-        System.err.println(from.length + " chars, length " + memorySize + " At " + newMemory);
+        // System.err.println(from.length + " chars, length " + memorySize + " At " + newMemory);
 
         char[] to = new char[256];
 
-        unsafe.copyMemory(from, MUnsafe.charArrayBaseOffset, null, newMemory, memorySize);
-        unsafe.copyMemory(from, MUnsafe.charArrayBaseOffset, to, MUnsafe.charArrayBaseOffset, memorySize);
+        long fromAddress = MUnsafe.toAddress(from);
 
-        System.err.println("copied " + to.length);
-        System.err.println(new String(to));
+        // unsafe.copyMemory(fromAddress + MUnsafe.charArrayBaseOffset, newMemory, memorySize);
+        // unsafe.copyMemory(from, MUnsafe.charArrayBaseOffset, null, newMemory, memorySize);
+        // unsafe.copyMemory(from, MUnsafe.charArrayBaseOffset, to, MUnsafe.charArrayBaseOffset, memorySize);
+
+        // System.err.println("copied " + to.length);
+        // System.err.println(new String(to));
         System.err.println("efg");
+        System.out.println("====\n");
 
-        for (int i = 0; i < memorySize; i++) {
-            // char c = unsafe.getChar(newMemory, i * MUnsafe.charArrayIndexScale);
-            int c = unsafe.getByte(newMemory, (long) i);
-            System.err.println(Integer.toHexString((byte) c) + " " + c);
+        for (int i = 0; i < memorySize + 512; i += 8) {
+            // char c = unsafe.getChar(newMemory, MUnsafe.charArrayBaseOffset + i * MUnsafe.charArrayIndexScale);
+            // char c2 = unsafe.getChar(to, MUnsafe.charArrayBaseOffset + (i * MUnsafe.charArrayIndexScale));
+            // byte c = unsafe.getByte(newMemory, (long) i);
+            long l = unsafe.getLong(newMemory, MUnsafe.normalize(i));
+            // System.out.println("c1 : " + Integer.toHexString((byte) c) + " " + c);
+            System.out.println("c1 : " + Long.toHexString(l) + " " + l);
+            // System.out.println("c2 : " + Integer.toHexString((byte) c2) + " " + c2);
             // System.err.println(c);
+            // System.out.print(c);
         }
         System.err.println("done");
 
         unsafe.freeMemory(newMemory);
     }
 
-    static class MUnsafe {
-        static final Unsafe unsafe;
-        public static final long charArrayBaseOffset;
-        public static final long charArrayIndexScale;
 
-        static {
-            try {
-                Field f = Unsafe.class.getDeclaredField("theUnsafe");
-                f.setAccessible(true);
-                unsafe = (Unsafe) f.get(null);
-                charArrayBaseOffset = unsafe.arrayBaseOffset(char[].class);
-                charArrayIndexScale = unsafe.arrayIndexScale(char[].class);
-            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static Unsafe getUnsafe() {
-            return unsafe;
-        }
-
-        public static long charArrayOffset(int index) {
-            return calculateOffset(index, charArrayBaseOffset, charArrayIndexScale);
-        }
-
-        public static long charArraySize(char[] from) {
-            return from.length * charArrayIndexScale;
-        }
-
-        /**
-         * The object referred to by o is an array, and the offset is an integer of the form B+N*S, where N is a valid
-         * index into the array, and B and S are the values obtained by #arrayBaseOffset and #arrayIndexScale
-         * (respectively) from the array's class. The value referred to is the Nth element of the array.
-         **/
-        public static long calculateOffset(int index, long base, long scale) {
-            return (base + (index * scale));
-        }
-
-        static long toAddress(Object obj) {
-            Object[] array = new Object[] {obj};
-            long baseOffset = getUnsafe().arrayBaseOffset(Object[].class);
-            return normalize(getUnsafe().getInt(array, baseOffset));
-        }
-
-        static Object fromAddress(long address) {
-            Object[] array = new Object[] {null};
-            long baseOffset = getUnsafe().arrayBaseOffset(Object[].class);
-            getUnsafe().putLong(array, baseOffset, address);
-            return array[0];
-        }
-
-        private static long normalize(int value) {
-            if (value >= 0)
-                return value;
-            return (~0L >>> 32) & value;
-        }
-
-
-    }
 }
